806bf6742e94f873fdb1df51e7ee6ed83a4d229b
Akshaysingh2004 <136893895+Akshaysingh2004@users.noreply.github.com>
2024-09-03 21:46:26 +0530

    Update MainPage.js

--- Components/MainPage.js
+++ Components/MainPage.js
@@ -1,18 +1,52 @@
-import React from 'react';
-import { Button, Container, Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Container, Modal } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import { FaUser, FaSignOutAlt, FaInfoCircle, FaUserCircle } from 'react-icons/fa';
 
 const MainPage = ({ handleSignOut }) => {
   const user = useSelector(state => state.user);
+  const [showProfileModal, setShowProfileModal] = useState(false);
+
+  const toggleProfileModal = () => {
+    setShowProfileModal(!showProfileModal);
+  };
 
   return (
-    <Container style={{ width: "30rem", height: "32rem", textAlign: "center" }}>
-      <Card>
-        <Card.Body>
-          <Card.Title className='fw-bold'>Welcome, {user?.username}</Card.Title>
-          <Button variant="danger" onClick={handleSignOut}>Sign Out</Button>
-        </Card.Body>
-      </Card>
+    <Container style={{ position: "relative", width: "100%", height: "100vh", padding: "2rem" }}>
+     
+      <Button 
+        variant="primary" 
+        onClick={toggleProfileModal}
+        style={{ position: "absolute", top: "20px", left: "20px" }}
+      >
+        <FaUserCircle /> Profile
+      </Button>
+<h2 class="text-center text-primary fw-bol">WELCOME</h2>
+     
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100%" }}>
+        <p>
+          This is your main dashboard. Here you can manage your account, view recent activities, and explore new features.
+        </p>
+        
+        <Button variant="danger" onClick={handleSignOut} className="mt-3">
+          <FaSignOutAlt /> Sign Out
+        </Button>
+      </div>
+
+     
+      <Modal show={showProfileModal} onHide={toggleProfileModal} centered>
+        <Modal.Header closeButton>
+          <Modal.Title><FaUserCircle />My Profile</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <p style={{ fontWeight: 'bold' }}> Username: {user?.username}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={toggleProfileModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
